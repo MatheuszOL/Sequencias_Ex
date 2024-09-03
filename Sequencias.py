529c60d02e320db9388c4951a41875c37b62a722
Matheus Pereira <mathp874@gmail.com>
2024-09-03 14:04:26 -0300

    Primeira commit

---
+++ Sequencias.py
@@ -0,0 +1,22 @@
+def sequencias():
+    # a) Sequência de números ímpares
+    print(f"Próximo número da sequência a: {7 + 2}")
+
+    # b) sequência de potências 
+    print(f"Próximo número da sequência b: {64 * 2}")
+
+    # c) quadrados perfeitos
+    print(f"Próximo número da sequência c: {36 + 2 * 7 + 1}")
+
+    # d) quadrados de números pares
+    print(f"Próximo número da sequência d: {64 + 2 * 8 + 4}")
+
+    # e) sequência de Fibonacci
+    a, b = 5, 8
+    print(f"Próximo número da sequência e: {a + b}")
+
+    # f) sequência com incrementos variados
+    print(f"Próximo número da sequência f: {19 + 1}")
+
+# Chamar a função para responder
+sequencias()
